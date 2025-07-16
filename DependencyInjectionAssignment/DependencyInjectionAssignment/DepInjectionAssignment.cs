5b5943ef9685207896650999e3076daee74b6c11
Sanyi93 <105607253+Sanyi93@users.noreply.github.com>
2025-07-16 17:05:23 +0200

    Add files via upload
    DependencyInjection Sample added

---
+++ DependencyInjectionAssignment/DependencyInjectionAssignment/DepInjectionAssignment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionAssignment
+{
+    internal class DepInjectionAssignment
+    {
+
+
+    }
+
+    class UserInputProcessor
+    {
+        private IDataProcessor _processor;
+
+        public UserInputProcessor(IDataProcessor processor)
+        {
+            _processor = processor;
+        }
+
+        public void processTheInput()
+        {
+
+            _processor.processData(Console.ReadLine());
+        }
+
+    }
+
+    interface IDataProcessor
+    {
+        void processData(string data)
+        {
+
+        }
+    }
+
+    class DataProcessingService :IDataProcessor
+    {
+        public void processData(string data)
+        {
+         
+            Console.WriteLine(data.ToUpper());
+        }
+
+    }
+}
---
+++ DependencyInjectionAssignment/DependencyInjectionAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DependencyInjectionAssignment;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Write some message please!");
+
+var processor1 = new DataProcessingService();
+var userInputProcessor1 = new UserInputProcessor(processor1);
+
+userInputProcessor1.processTheInput();
